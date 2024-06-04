49992879c3ee5e69e76449d80c776196a5d7c439
VictorRubinec <victor.rubinec@sptech.school>
2024-06-04 15:09:44 -0300

    webcrawler refeito e adicionado o wordcloud

--- twitter/app.py
+++ twitter/app.py
@@ -5,21 +5,26 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from unidecode import unidecode
 
 # lexico
-from leia import SentimentIntensityAnalyzer
+from leia.leia import SentimentIntensityAnalyzer
 
 # grafico
 import matplotlib.pyplot as plt
+
+# wordcloud
+from wordcloud import WordCloud
 
 class App:
     def __init__(self):
         pass
         
     def get_base(self):
-        with open('aplicacao/config/base.json') as f:
+        with open('twitter/config/base.json') as f:
             self.base = json.load(f)
 
     def levenshtein(self, s1, s2):
@@ -59,32 +64,45 @@ class App:
     
     def search_twitter(self, term):
         driver = webdriver.Chrome()
-        
         driver.get("https://x.com/login")
-        time.sleep(3)
-        username = driver.find_element(By.CLASS_NAME, "r-30o5oe")
+        
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//input[@name="text"]'))
+        )
+        
+        username = driver.find_element(By.XPATH, '//input[@name="text"]')
         username.send_keys('shift534')
         username.send_keys(Keys.RETURN)
-        time.sleep(3)
-
-        password = driver.find_element(By.NAME, "password")
-        password.send_keys('MIrai123@')
+        
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//input[@name="password"]'))
+        )
+        
+        password = driver.find_element(By.XPATH, '//input[@name="password"]')
+        password.send_keys('MIrai123@') 
         password.send_keys(Keys.RETURN)
-        time.sleep(3)
+        
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//input[@class="r-30o5oe r-1dz5y72 r-1niwhzg r-17gur6a r-1yadl64 r-deolkf r-homxoj r-poiln3 r-7cikom r-1ny4l3l r-xyw6el r-13qz1uu r-fdjqy7"]'))
+        )
         
         driver.get("https://x.com/search?q=" + term)
-
-        time.sleep(5)
+        
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.LINK_TEXT, "Latest"))
+        )
+        
         latest_tab = driver.find_element(By.LINK_TEXT, "Latest")
         latest_tab.click()
-        
-        time.sleep(5)
         
         tweets = []
         
         for i in range(10):
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.TAG_NAME, "article"))
+            )
+            
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(5)
             
             soup = BeautifulSoup(driver.page_source, 'html.parser')
 
@@ -92,9 +110,9 @@ class App:
             
             for b in bloco:
                 tweets.append(b)
-
+        
         data = { 'tweets': [] }
-
+        
         for tweet in tweets:
             tweet_text = tweet.find('div', {'lang': True})
             if tweet_text:
@@ -113,10 +131,10 @@ class App:
             
         driver.close()
         
-        with open('aplicacao/config/tokens.json', 'w') as f:
+        with open('twitter/config/tokens.json', 'w') as f:
             json.dump(data, f, indent=4)
             
-        with open('aplicacao/config/tokens.json') as f:
+        with open('twitter/config/tokens.json') as f:
             self.tokens = json.load(f)
         
     def lexico(self):
@@ -142,10 +160,10 @@ class App:
             
             lista_tokens.append(tokens)
             
-        with open('aplicacao/config/tokens_lexico.json', 'w', encoding='utf-8') as file:
+        with open('twitter/config/tokens_lexico.json', 'w', encoding='utf-8') as file:
             json.dump(lista_tokens, file, indent=4)
             
-        with open('aplicacao/config/tokens_lexico.json') as f:
+        with open('twitter/config/tokens_lexico.json') as f:
             self.tokens_lexico = json.load(f)
         
     def exibir_grafico(self):
@@ -173,6 +191,17 @@ class App:
         plt.xlabel('Sentimento')
         plt.ylabel('Número de Tweets')
         plt.title('Distribuição de Sentimentos')
+        plt.savefig('twitter/resultados/grafico.png')
+        plt.show()
+    
+    def exibir_wordcloud(self):
+        text = ' '.join([entry['tweet'] for entry in self.tokens_lexico])
+        
+        wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+        
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis('off')
+        plt.savefig('twitter/resultados/wordcloud.png')
         plt.show()
     
     def run(self):
@@ -190,15 +219,17 @@ class App:
                 'text': self.corrigir_frase(frase['text'])
             })
         
-        with open('aplicacao/config/tokens_tratados.json', 'w') as f:
+        with open('twitter/config/tokens_tratados.json', 'w') as f:
             json.dump(frases_tratadas, f, indent=4)
             
-        with open('aplicacao/config/tokens_tratados.json') as f:
+        with open('twitter/config/tokens_tratados.json') as f:
             self.tokens_tratados = json.load(f)
         
         self.lexico()
 
-        self.exibir_grafico()        
+        self.exibir_grafico()      
+        
+        self.exibir_wordcloud()  
     
 def main():
     app = App()
